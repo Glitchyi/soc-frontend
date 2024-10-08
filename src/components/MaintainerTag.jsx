eba0ddb692a6b932389afcffe981860a82adb799
Rovn <thomjoh995@gmail.com>
2024-10-08 21:37:54 +0530

    maintainers done ig

--- src/components/MaintainerTag.jsx
+++ src/components/MaintainerTag.jsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line react/prop-types
 export default function MaintainerTag({ avatar = "", github = "", link = "" }) {
   return (
-    <div className={"pt-sans flex items-center justify-between flex-wrap border-2 border-darkpacha/20 px-4 py-4 rounded-lg"}>
+    <div className={"pt-sans flex items-center justify-between flex-wrap border-4 border-darkpacha/80 px-4 py-4 rounded-lg"}>
       <div className={"flex items-center gap-8"}>
         <img src={avatar} className={"w-20 rounded-full"} />
         <div className="flex flex-col gap-2">
