adf3c97a5c4cd26b14835e3fed0d76d85a17b057
Glitchyi <advaith@glitchy.systems>
2024-12-27 18:39:55 +0530

    Yes

--- src/Maintainers.jsx
+++ src/Maintainers.jsx
@@ -6,62 +6,59 @@ const Maintainers = () => {
     {
       github: "Advaith Narayanan",
       avatar: "https://avatars.githubusercontent.com/u/55801439?v=4",
-      link: "https://github.com/Glitchyi"
+      link: "https://github.com/Glitchyi",
     },
     {
-        github: "Rohit Babu George",
-        avatar: "https://avatars.githubusercontent.com/u/86565903?v=4",
-        link: "https://github.com/Xrg360"
-      },
-      {
-        github: "Rovan Thomas John",
-        avatar: "https://avatars.githubusercontent.com/u/128726991?v=4",
-        link: "https://github.com/rovxn"
-      },
-      {
-        github: "Adrin Jose C T",
-        avatar: "https://avatars.githubusercontent.com/u/112411286?v=4",
-        link: "https://github.com/Adrin7113"
-      },
-      {
-          github: "Bhavan S",
-          avatar: "https://avatars.githubusercontent.com/u/109290304?v=4",
-          link: "https://github.com/parzi-al"
-        },
-        {
-          github: "Joshua Varghese",
-          avatar: "https://avatars.githubusercontent.com/u/69136448?v=4",
-          link: "https://github.com/infinitytmbots"
-        },
-        {
-            github: "Vyshnav D Kumar",
-            avatar: "https://avatars.githubusercontent.com/u/107564336?v=4",
-            link: "https://github.com/vyshnvdkmr"
-          },
-        {
-            github: "Vaishakh S Nair",
-            avatar: "https://avatars.githubusercontent.com/u/36193436?v=4",
-            link: "https://github.com/vaishakhsnair"
-          },
-            {
-              github: "Amalekh Biju",
-              avatar: "https://avatars.githubusercontent.com/u/97168630?v=4",
-              link: "https://github.com/Amalekh-Biju"
-            },
+      github: "Rohit Babu George",
+      avatar: "https://avatars.githubusercontent.com/u/86565903?v=4",
+      link: "https://github.com/Xrg360",
+    },
+    {
+      github: "Rovan Thomas John",
+      avatar: "https://avatars.githubusercontent.com/u/128726991?v=4",
+      link: "https://github.com/rovxn",
+    },
+    {
+      github: "Adrin Jose C T",
+      avatar: "https://avatars.githubusercontent.com/u/112411286?v=4",
+      link: "https://github.com/Adrin7113",
+    },
+    {
+      github: "Bhavan S",
+      avatar: "https://avatars.githubusercontent.com/u/109290304?v=4",
+      link: "https://github.com/parzi-al",
+    },
+    {
+      github: "Joshua Varghese",
+      avatar: "https://avatars.githubusercontent.com/u/69136448?v=4",
+      link: "https://github.com/infinitytmbots",
+    },
+    {
+      github: "Vyshnav D Kumar",
+      avatar: "https://avatars.githubusercontent.com/u/107564336?v=4",
+      link: "https://github.com/vyshnvdkmr",
+    },
+    {
+      github: "Vaishakh S Nair",
+      avatar: "https://avatars.githubusercontent.com/u/36193436?v=4",
+      link: "https://github.com/vaishakhsnair",
+    },
+    {
+      github: "Amalekh Biju",
+      avatar: "https://avatars.githubusercontent.com/u/97168630?v=4",
+      link: "https://github.com/Amalekh-Biju",
+    },
   ];
 
   return (
-    <div
-      className=" h-screen flex flex-col justify-between"
-      id="background"
-    >
+    <div className=" h-screen flex flex-col justify-between" id="background">
       <div className="mx-10 mt-10 gap-10 flex flex-col">
         <Header />
         <h1 className="text-6xl underline underline-offset-4 font-bold text-darkpacha">
           Maintainers
         </h1>
-                <div className="px-10 flex justify-center">
-          <div className="grid grid-cols-3 gap-10">
+        <div className="flex">
+          <div className="lg:grid flex flex-col overflow-auto grid-cols-3 w-full gap-10">
             {data.map((maintainer, index) => (
               <MaintainerTag
                 key={index}
