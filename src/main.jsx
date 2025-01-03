dbdccbb5fc57e26ca1670d00c82a2e7865f1655e
Adrin Jose C T <adrinjosect@gmail.com>
2025-01-03 16:05:11 +0530

    Working Oauth

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 import Template from "./layout/Template";
 
 import "./index.css";
@@ -13,21 +14,25 @@ import Dashboard from "./pages/dashboard/Dashboard";
 import Sign_Up from "./pages/sign_up/Sign_Up";
 // import ComingSoon from "./ComingSoon";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Template>
-      <Router>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/guidelines" element={<Guidelines />} />
-          <Route path="/maintainers" element={<Maintainers />} />
-          <Route path="/repositories" element={<Repositories />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/sign_up" element={<Sign_Up />} />
-          {/* <Route path="/" element={<ComingSoon />} /> */}
-        </Routes>
-      </Router>
-    </Template>
+    <QueryClientProvider client={queryClient}>
+      <Template>
+        <Router>
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/guidelines" element={<Guidelines />} />
+            <Route path="/maintainers" element={<Maintainers />} />
+            <Route path="/repositories" element={<Repositories />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/sign_up" element={<Sign_Up />} />
+            {/* <Route path="/" element={<ComingSoon />} /> */}
+          </Routes>
+        </Router>
+      </Template>
+    </QueryClientProvider>
   </StrictMode>
 );
--- src/pages/sign_up/Sign_Up.jsx
+++ src/pages/sign_up/Sign_Up.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useQuery } from "react-query";
+import axios from "axios";
 import Header from "../../components/Header";
+import { use } from "react";
+
+const fetchData = async () => {
+  const response = await axios.get("http://127.0.0.1:5000/login");
+  return response.data;
+};
 
 const Sign_Up = () => {
+  const { data, error, isLoading, refetch } = useQuery("fetchData", fetchData, {
+    enabled: false, // Disable automatic query execution
+  });
+  const handleClick = () => {
+    refetch();
+  };
+  useEffect(() => {
+    if (data) {
+        
+    }
+  }, [data]);
   return (
     <div className="p-12 h-screen">
       <Header
@@ -11,8 +30,11 @@ const Sign_Up = () => {
 
       <div className=" h-full flex items-center">
         <div className="w-1/2 h-[80%] border-r-2 border-black/60 flex flex-col items-center justify-center gap-2">
-          <button className="font-bold text-2xl text-white bg-black py-4 px-6 rounded-full hover:scale-105 transition-all ease-in-out">
-            Register with Github
+          <button
+            onClick={handleClick}
+            className="font-bold text-2xl text-white bg-black py-4 px-6 rounded-full hover:scale-105 transition-all ease-in-out"
+          >
+            {isLoading ? "Loading..." : "Register with Github"}
           </button>
           <span>
             or <span className="underline cursor-pointer">sign back in</span>
